261bcab13e993076aeb2300e5931390a4df1f645
StuartCowley <voidstalker@live.co.uk>
2021-06-22 20:57:40 +0100

    fix non loading routes, add clickable links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Switch>
-        <Route path="/">
-          <h1>Home</h1>
-        </Route>
-        <Route path="/products">
-          <h1>Products</h1>
-        </Route>
-        <Route path="/about">
-          <h1>About</h1>
-        </Route>
-        <Route path="/contact-us">
-          <h1>Contact us</h1>
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <h1>Example shop</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                Go to homepage
+              </Link>
+            </li>
+            <li>
+              <Link to="/products">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact-us">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route exact path="/products">
+            <h1>Products</h1>
+          </Route>
+          <Route exact path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route exact path="/contact-us">
+            <h1>Contact us</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
