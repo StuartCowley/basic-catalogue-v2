0c58c3d20f7c32e3048886a06030ae81f2ab391e
StuartCowley <voidstalker@live.co.uk>
2021-06-22 20:54:00 +0100

    introduce switch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Router>
-        <Route path="/">
-          <h1>Home</h1>
-        </Route>
-        <Route path="/products">
-          <h1>Products</h1>
-        </Route>
-        <Route path="/about">
-          <h1>About</h1>
-        </Route>
-        <Route path="/contact-us">
-          <h1>Contact us</h1>
-        </Route>
+        <Switch>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route path="/products">
+            <h1>Products</h1>
+          </Route>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/contact-us">
+            <h1>Contact us</h1>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
