1f2d3f0ae41b82398df9bfc1adae03bc850758b0
StuartCowley <voidstalker@live.co.uk>
2021-06-22 21:00:53 +0100

    Replace Links with NavLinks, add basic style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
+
+import './styles/app.css';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
@@ -9,24 +12,24 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">
+              <NavLink activeClassName="selected-link" exact to="/">
                 Go to homepage
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/products">
+              <NavLink activeClassName="selected-link" to="/products">
                 Products
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/about">
+              <NavLink activeClassName="selected-link" to="/about">
                 About
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/contact-us">
+              <NavLink activeClassName="selected-link" to="/contact-us">
                 Contact
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </nav>
