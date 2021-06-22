14f6d1e3ae5b4881e3628eb1450da5256757ad3e
StuartCowley <voidstalker@live.co.uk>
2021-06-22 20:49:20 +0100

    install router, create basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      
+    <Router>
+      <Route path="/">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/products">
+        <h1>Products</h1>
+      </Route>
+      <Route path="/about">
+        <h1>About</h1>
+      </Route>
+      <Route path="/contact-us">
+        <h1>Contact us</h1>
+      </Route>
+    </Router>
     </div>
   );
 }
