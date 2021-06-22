b13f70c4056d5d919ea52ff937a9700df2362658
StuartCowley <voidstalker@live.co.uk>
2021-06-22 20:57:40 +0100

    fix non loading routes, add clickable links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Router>
+        <h1>Example shop</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                Go to homepage
+              </Link>
+            </li>
+            <li>
+              <Link to="/products">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact-us">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <h1>Home</h1>
           </Route>
-          <Route path="/products">
+          <Route exact path="/products">
             <h1>Products</h1>
           </Route>
-          <Route path="/about">
+          <Route exact path="/about">
             <h1>About</h1>
           </Route>
-          <Route path="/contact-us">
+          <Route exact path="/contact-us">
             <h1>Contact us</h1>
           </Route>
         </Switch>
