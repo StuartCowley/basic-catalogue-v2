71182d47ed38a50530004f3635519d4011ffbeb2
StuartCowley <voidstalker@live.co.uk>
2021-10-17 13:52:17 +0100

    Add hooks and navigation buttons to productDetails

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 
 import products from "../data/products.json";
 
@@ -7,6 +12,16 @@ import "../styles/product-details.css";
 
 const ProductDetails = () => {
   const { id } = useParams();
+
+  const history = useHistory();
+  const location = useLocation();
+  const match = useRouteMatch();
+
+  console.log(match.params);
+  console.log(match.isExact);
+  console.log(match.path);
+  console.log(match.url);
+  console.log(location);
 
   const selectedProduct = products.find(
     (product) => product.id === parseInt(id)
@@ -24,6 +39,20 @@ const ProductDetails = () => {
         <div>Weight: {selectedProduct.weight}g</div>
         <div>£{selectedProduct.price.toFixed(2)}</div>
       </div>
+      <button
+        onClick={() => {
+          history.goBack();
+        }}
+      >
+        Go back
+      </button>
+      <button
+        onClick={() => {
+          history.goForward();
+        }}
+      >
+        Forward
+      </button>
     </div>
   );
 };
