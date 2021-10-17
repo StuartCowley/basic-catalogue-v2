39a019eefc6753cd8cac679d1e7d332bb060440d
StuartCowley <voidstalker@live.co.uk>
2021-10-17 13:47:36 +0100

    use useParams to get id from route and fill out details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import products from "../data/products.json";
 
 import "../styles/product-details.css";
 
 const ProductDetails = () => {
+  const { id } = useParams();
+
+  const selectedProduct = products.find(
+    (product) => product.id === parseInt(id)
+  );
+
   return (
     <div className="product-list-wrap">
       <div className="product-item-wrap">
         <div className="product-list-header">
-          <div>Selected product</div>
+          <div>{selectedProduct.title}</div>
         </div>
+        <div>Rating: {selectedProduct.rating}</div>
+        <div>Description: {selectedProduct.description}</div>
+        <div>Type: {selectedProduct.type}</div>
+        <div>Weight: {selectedProduct.weight}g</div>
+        <div>£{selectedProduct.price.toFixed(2)}</div>
       </div>
     </div>
   );
