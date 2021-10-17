3a5f11087f2c33a57ccebcb4254f032f7b7821b6
StuartCowley <voidstalker@live.co.uk>
2021-10-17 13:33:02 +0100

    Fill out ProductList component and add styles

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/product-list.css"
 
 const ProductList = ({ products }) => {
   return (
-    <div>
+    <div className="product-list-wrap">
       {products.map((product) => {
         return (
-          <div>{product.title}</div>
+          <div key={product.id} className="product-item-wrap">
+            <div className="product-list-header">
+              <div>{product.title}</div>
+              <div>{product.rating}</div>
+            </div>
+            <div>{product.description}</div>
+            <Link to={`product/${product.id}`}>Details</Link>
+          </div>
         );
       })}
     </div>
